7c6f068d859a6fff39b46ef258d9ce566d7af0f6
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-03-31 19:13:00 +0530

    all

---
+++ script.js
@@ -0,0 +1,32 @@
+let stars = document.getElementById('stars')
+let moon = document.getElementById('moon')
+let mountains_behind = document.getElementById('mountains_behind')
+let mountains_front = document.getElementById('mountains_front')
+let text = document.getElementById('text')
+let btn = document.getElementById('btn')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY
+     stars.style.left = value * 1.25 + 'px';
+     moon.style.top = value * 1.5 + 'px';
+     mountains_behind.style.top = value * 0.5 + 'px';
+     mountains_front.style.top = value * 0 + 'px';
+     text.style.marginRight = value * 4 + 'px'
+     text.style.marginTop = value * 1.5 + 'px'
+     btn.style.marginTop = value * 1.5 + 'px'
+})
+function playAnimation() {
+    const container = document.getElementById('lottie-container');
+    container.style.display = 'block'; // Show the animation container
+
+    lottie.loadAnimation({
+        container: container,  // the dom element to render the animation
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'Animation - 1743427099440.json' // Update with the path to your Lottie JSON file
+    });
+}
+
+
+
